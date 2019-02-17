a8db094d9c5ff94fefed3f16fc4d2132581385c3
Josh Hepworth <jrhepworth12@gmail.com>
2019-02-17 14:13:37 -0600

    initial controller setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+CSV.foreach('crm_exercise_data.csv') do |row|
+  name = row[0]
+  job_title = row[1]
+  email_address = row[2]
+  phone = row[3]
+  organization = row[4]
+  organization_phone = row[5]
+  domain = row[6]
+  street = row[7]
+  city = row[8]
+  state = row[9]
+  zip = row[10] 
+end
+puts "done"
