260977a365b53dbb74c87bb0d489dc9397a7e913
Josh Hepworth <jrhepworth12@gmail.com>
2019-02-27 14:56:01 -0600

    adjusting relations for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-belongs_to :organization
-has_one :address
+	belongs_to :organization
+	has_one :address
+  
+  def people_count
+    people_count = User.where(organization_id: id)
+    people_count.length
+  end
 end
